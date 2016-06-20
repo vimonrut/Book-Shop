3f8a553661035b9c404a7d42a04d58718ac000e2
vimonrut <15nokmimee@gmail.com>
2016-06-20 17:23:27 +0700

    finish 20june

---
+++ app/src/main/java/neu/sunoontong/vimnorut/bookshop/MyAlert.java
@@ -0,0 +1,29 @@
+package neu.sunoontong.vimnorut.bookshop;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+/**
+ * Created by iMac_25 on 6/20/2016 AD.
+ */
+public class MyAlert {
+
+    public void myDialog(Context context,
+                         String strTitle,
+                         String strMessage) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setCancelable(false);
+        builder.setIcon(R.drawable.bird48);
+        builder.setTitle(strTitle);
+        builder.setMessage(strMessage);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
+    }
+
+} // Main Class
--- app/src/main/java/neu/sunoontong/vimnorut/bookshop/SignUpActivity.java
+++ app/src/main/java/neu/sunoontong/vimnorut/bookshop/SignUpActivity.java
@@ -2,12 +2,61 @@ package neu.sunoontong.vimnorut.bookshop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //Explicit ประกาศตัวแปร
+    private EditText nameEditText,userEditText,passwordEditText;
+    private String nameString, userString, passwordString;
+    private static final String urlPHP = "http://swiftcodingthai.com/neu/add_user_master.php";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //Bind Widget
+        nameEditText = (EditText) findViewById(R.id.editText);
+        userEditText = (EditText) findViewById(R.id.editText2);
+        passwordEditText = (EditText) findViewById(R.id.editText3);
+
+
+    } //Main method
+
+    public void clickSingUpSing(View view){
+
+        nameString = nameEditText.getText().toString().trim();
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        // Check Space
+        if (nameString.equals("") || userString.equals("") || passwordString.equals("") ) {
+           //Have Space
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this, "มีช่องว่าง", "กรุณากรอกข้อความ");
+
+
+
+        } else {
+          //No Space
+            uploadToserver(); 
+                
+            
+            
+
+
+
+        }
+
+
+
+    } // clickSing
+
+    private void uploadToserver() {
     }
-}
+
+}   // Main class
