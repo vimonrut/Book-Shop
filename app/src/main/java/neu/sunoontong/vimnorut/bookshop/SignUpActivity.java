e8593086b1f7cfd6e5a2b31ee3853f3443ad85f5
vimonrut <15nokmimee@gmail.com>
2016-06-21 11:58:06 +0700

    Dessing

--- app/src/main/java/neu/sunoontong/vimnorut/bookshop/SignUpActivity.java
+++ app/src/main/java/neu/sunoontong/vimnorut/bookshop/SignUpActivity.java
@@ -2,8 +2,19 @@ package neu.sunoontong.vimnorut.bookshop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.ActionMode;
 import android.view.View;
 import android.widget.EditText;
+
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -57,6 +68,34 @@ public class SignUpActivity extends AppCompatActivity {
     } // clickSing
 
     private void uploadToserver() {
-    }
+
+        OkHttpClient okHttpClient = new OkHttpClient();
+        RequestBody requestBody = new FormEncodingBuilder()
+                .add("isAdd", "true")
+                .add("Name", nameString)
+                .add("User", userString)
+                .add("Password", passwordString)
+                .build();
+        Request.Builder builder = new Request.Builder();
+        Request request = builder.url(urlPHP).post(requestBody).build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                finish();
+
+            }
+        });
+
+
+
+
+
+    } // upload
 
 }   // Main class
